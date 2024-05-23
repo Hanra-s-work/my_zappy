843133d77a886621aa2b0ba5b865b5ff2878ecf7
Henry LETELLIER <henry.letellier@epitech.eu>
2024-05-23 04:05:38 +0200

    [ADD] files in the server folder so that it could compile

---
+++ src/server/include/src.h
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2023
+** my_zappy [fed34_prox_vm]
+** File description:
+** src.h
+*/
+
+#pragma once
+int sub_main(int argc, char **argv);
---
+++ src/server/src/sub_main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** my_zappy [fed34_prox_vm]
+** File description:
+** sub_main.c
+*/
+
+#include <stdio.h>
+
+int sub_main(int argc, char **argv)
+{
+    if (argc == 2) {
+        printf("%s", argv[1]);
+    }
+    return 0;
+}
