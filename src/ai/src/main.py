e14b1e7e39ef9c8a40d81e8eab93198e7e433b93
Henry LETELLIER <henry.letellier@epitech.eu>
2024-05-29 01:21:55 +0200

    [ADD] help section for the AI binary

--- src/ai/src/main.py
+++ src/ai/src/main.py
@@ -6,5 +6,12 @@
 ##
 """! @brief Example program for compilation reasons """
 
+import sys
+
+
+if len(sys.argv) == 2 and sys.argv[1] == "-help":
+    print("USAGE: ./zappy_ai -p port -n name -h machine")
+    sys.exit(0)
+
 """! Simple function to display the string 'Hello World!'"""
 print("Hello World!")
