a6c2499e44c60f9406a76a33697c4c97d7fbcf41
Eric XU <eric1.xu@epitech.eu>
2024-06-07 11:44:36 +0200

    [UPDATE] server.h file name to main.h

--- src/server/include/main.h
+++ src/server/include/main.h
@@ -5,16 +5,17 @@
 ** server.h
 */
 
-#pragma once
-
-#include "struct.h"
+#ifndef MAIN_H
+    #define MAIN_H
 
 /**
 ** @brief This is the main function of the program
 ** (this is the function called by the main used for compiling)
 **
 ** @param argc
-** @param argv 
+** @param argv
 ** @return int
 **/
 int sub_main(int argc, char **argv);
+
+#endif
