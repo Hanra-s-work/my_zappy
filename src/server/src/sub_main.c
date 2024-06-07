fb44565b292668ba87e51fd460d624d46cce9ba3
Eric XU <eric1.xu@epitech.eu>
2024-06-07 12:06:41 +0200

    [ADD] server helper and use argument getter

--- src/server/src/sub_main.c
+++ src/server/src/sub_main.c
@@ -5,12 +5,41 @@
 ** sub_main.c
 */
 
+#include <string.h>
 #include <stdio.h>
+
+#include "arg_parse.h"
+#include "utils.h"
+
+static int display_helper(void)
+{
+    printf("USAGE:\n"
+    "\t./zappy_server -p port -x width -y height -n name1 name2 ..."
+    "-c clientNb -f freq\n\n"
+    "DESCRIPTION:\n"
+    "\t-p port             port number\n"
+    "\t-x width            width of the world\n"
+    "\t-y height           height of the world\n"
+    "\t-n name1 name2 ...  name of the team\n"
+    "\t-c clientNb         number of authorized clients per team\n"
+    "\t-f freq             reciprocal of time unit for execution of actions\n");
+    return (0);
+}
 
 int sub_main(int argc, char **argv)
 {
-    if (argc == 2) {
-        printf("%s", argv[1]);
+    struct arg_s **arguments = NULL;
+
+    if (argc == 2 &&
+    (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 ||
+    strcmp(argv[1], "--help") == 0)) {
+        return display_helper();
     }
-    return 0;
+    arguments = get_zappy_args(argc, (const char **)argv, SERVER_OPTION);
+    if (arguments == NULL) {
+        display_helper();
+        return (ERROR);
+    }
+    free_args(arguments, (argc / 2));
+    return (0);
 }
