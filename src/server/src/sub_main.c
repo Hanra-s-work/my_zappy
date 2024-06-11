0669fac6ceaeed4a1adadd9093454d808cae7ba3
Eric XU <eric1.xu@epitech.eu>
2024-06-11 23:24:21 +0200

    [ADD] server initialization to the main

--- src/server/src/sub_main.c
+++ src/server/src/sub_main.c
@@ -5,16 +5,19 @@
 ** sub_main.c
 */
 
-#include <string.h>
+#include <time.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
 
-#include "arg_parse.h"
 #include "utils.h"
+#include "arg_parse.h"
+#include "client_management.h"
 
 static int display_helper(void)
 {
     printf("USAGE:\n"
-    "\t./zappy_server -p port -x width -y height -n name1 name2 ..."
+    "\t./zappy_server -p port -x width -y height -n name1 name2 ... "
     "-c clientNb -f freq\n\n"
     "DESCRIPTION:\n"
     "\t-p port             [MANDATORY] port number\n"
@@ -27,33 +30,54 @@ static int display_helper(void)
     return (0);
 }
 
-static int arguments_handler(int argc, char **argv, struct arg_s ***arguments)
+static int arguments_handler(int argc, char **argv, server_handler_t *server)
 {
+    struct arg_s **arguments = NULL;
     int nb_param = get_nb_parameter(argc, (const char **)argv);
 
-    *arguments = get_zappy_args(argc, (const char **)argv, SERVER_OPTION);
-    if (*arguments == NULL) {
-        write_error_msg("See the helper with '-h' or '-help' option.\n");
-        return (-1);
+    arguments = get_zappy_args(argc, (const char **)argv, SERVER_OPTION);
+    if (arguments == NULL) {
+        return write_error_msg("See the helper with '-help' option.\n");
     }
-    if (check_values_validity((const struct arg_s **)*arguments,
+    if (check_values_validity((const struct arg_s **)arguments,
     nb_param) == -1) {
         return (-1);
     }
-    free_args(*arguments, nb_param);
+    if (server_initialization(server, arguments) == -1) {
+        return (-1);
+    }
+    free_args(arguments, nb_param);
+    return (0);
+}
+
+static int shutdown_server(server_handler_t *server)
+{
+    if (server->game_data.map != NULL) {
+        free_array(server->game_data.map);
+    }
+    if (server->game_data.teams != NULL) {
+        for (int i = 0; server->game_data.teams[i].team_name != NULL; i++) {
+            free(server->game_data.teams[i].team_name);
+        }
+        free(server->game_data.teams);
+    }
+    delete_all_client(server->game_data.clients);
+    if (shutdown(server->socket, SHUT_RDWR) == -1) {
+        return (ERROR);
+    }
     return (0);
 }
 
 int sub_main(int argc, char **argv)
 {
-    struct arg_s **arguments = NULL;
+    server_handler_t server;
 
-    if (argc == 2 &&
-    (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0)) {
+    srand(time(NULL));
+    if (argc == 2 && strcmp(argv[1], "-help") == 0) {
         return display_helper();
     }
-    if (arguments_handler(argc, argv, &arguments) == -1) {
+    if (arguments_handler(argc, argv, &server) == -1) {
         return (ERROR);
     }
-    return (0);
+    return (shutdown_server(&server));
 }
