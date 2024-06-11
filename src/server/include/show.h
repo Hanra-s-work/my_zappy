f0702004a3f80af656938fa418a75027bf0eff84
Eric XU <eric1.xu@epitech.eu>
2024-06-11 23:23:25 +0200

    [ADD] functions to display games data

---
+++ src/server/include/show.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** my_zappy
+** File description:
+** show
+*/
+
+#ifndef SHOW_H_
+    #define SHOW_H_
+
+    #include "server_handler.h"
+
+int show_game_data(server_handler_t *server, bool need_to_show);
+
+#endif
