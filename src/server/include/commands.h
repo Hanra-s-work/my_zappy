f8d4d50a8a390c78fca703db35b75940662852ca
Victor Yvon <victor.yvon@epitech.eu>
2024-06-20 16:46:42 +0200

    [ADD](server/command/gui) player info commands

--- src/server/include/commands.h
+++ src/server/include/commands.h
@@ -11,11 +11,15 @@
     #include "server_handler.h"
 
     #define COMMAND_ID_LEN          3
-    #define MAP_SIZE_COMMMAND       "msz"
-    #define TEAM_NAME_COMMAND       "tna"
-    #define TILE_CONTENT_COMMAND    "bct"
-    #define TIME_UNIT_COMMAND       "sgt"
-    #define COMMAND_PARAMETER_ERROR "sbp"
+    #define MAP_SIZE_COMMMAND           "msz"
+    #define TEAM_NAME_COMMAND           "tna"
+    #define PLAYER_POSITION_COMMAND     "ppo"
+    #define PLAYER_LEVEL_COMMAND        "plv"
+    #define PLAYER_INVENTORY_COMMAND    "pin"
+    #define TILE_CONTENT_COMMAND        "bct"
+    #define TIME_UNIT_COMMAND           "sgt"
+    #define COMMAND_PARAMETER_ERROR     "sbp"
+
 
 int map_size_request(server_handler_t *server, char **args, const int idx);
 
@@ -33,6 +37,16 @@ int tile_content_request(server_handler_t *server, char **args, const int idx);
 
 
 map_t *get_tile_content(server_handler_t *server, int x, int y);
+
+
+int player_position_request(server_handler_t *server, char **args,
+    const int idx);
+
+int player_level_request(server_handler_t *server, char **args,
+    const int idx);
+
+int player_inventory_request(server_handler_t *server, char **args,
+    const int idx);
 
 
 int command_parameter_error(server_handler_t *server, char **args, const int
---
+++ src/server/src/commands/player_info.c
@@ -0,0 +1,97 @@
+/*
+** EPITECH PROJECT, 2023
+** my_zappy
+** File description:
+** player_info.c
+*/
+
+#include <unistd.h>
+#include "commands.h"
+
+int player_position_request(server_handler_t *server, char **args,
+    const int idx)
+{
+    write(server->game_data.clients[idx].fd, PLAYER_POSITION_COMMAND,
+        COMMAND_ID_LEN);
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .client_num, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .pos[0], sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .pos[1], sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_SEPARATOR,
+        1);
+    return (EXIT_SUCCESS);
+}
+
+int player_level_request(server_handler_t *server, char **args,
+    const int idx)
+{
+    write(server->game_data.clients[idx].fd, PLAYER_LEVEL_COMMAND,
+        COMMAND_ID_LEN);
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .client_num, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .level, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_SEPARATOR,
+        1);
+    return (EXIT_SUCCESS);
+}
+
+static void write_end_of_inv_content(server_handler_t *server, const int idx)
+{
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.deraumere_nb, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.sibur_nb, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.mendiane_nb, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.phiras_nb, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.thystame_nb, sizeof(int));
+}
+
+int player_inventory_request(server_handler_t *server, char **args,
+    const int idx)
+{
+    (void) args;
+    write(server->game_data.clients[idx].fd, PLAYER_INVENTORY_COMMAND,
+        COMMAND_ID_LEN);
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .client_num, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.food_nb, sizeof(int));
+    write(server->game_data.clients[idx].fd, COMMAND_DELIMITER,
+        1);
+    write(server->game_data.clients[idx].fd, server->game_data.clients[idx]
+        .ressources.linemate_nb, sizeof(int));
+    write_end_of_inv_content(server, idx);
+    write(server->game_data.clients[idx].fd, COMMAND_SEPARATOR,
+        1);
+    return (EXIT_SUCCESS);
+}
